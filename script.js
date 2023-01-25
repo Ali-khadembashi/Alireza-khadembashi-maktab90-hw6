b9d21a88c9217ea388e45bd296a873494056950b
Ali Khadembashi <ali.khadembashi@protonmail.com>
2023-01-25 22:24:53 +0330

    feature js

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+/*Task 1*/
+// let user = {
+//     name: 'John',
+//     age: 30,
+//     hello: function () {
+//         console.log(` hello ${this.name}`);
+//     }
+// };
+// user.hello();
